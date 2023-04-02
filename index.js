a32b27234b2e318bd31eca5d49ebe909fb44074b
attilamonteiro <atmaload@gmail.com>
2023-04-02 16:52:00 -0300

    att listen

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const app = require("./src/interfaces/server");
 
 
-app.listen(process.env.PORT ? Number(process.env.PORT) : 8080, '0.0.0.0', () => console.log("Servidor rodando!"));
+app.listen(process.env.PORT || 8080, () => console.log("Servidor rodando!"));

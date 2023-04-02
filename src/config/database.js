95d8d56dfde22ee160ae88edb32f921faf4c08b8
attilamonteiro <atmaload@gmail.com>
2023-04-02 17:15:00 -0300

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,8 @@ const Sequelize = require('sequelize');
 
 const connection = new Sequelize('guiaperguntas','root','123456',{
     host: '0.0.0.0',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    port:'3306'
 });
 
 module.exports = connection;

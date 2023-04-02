e3c66ef1f20d83f69ecad8c97c29ed948daf87c9
attilamonteiro <atmaload@gmail.com>
2023-04-01 22:51:21 -0300

    update

--- src/interfaces/server.js
+++ src/interfaces/server.js
@@ -14,16 +14,12 @@ const perguntaController = new PerguntaController({ obterPerguntasUseCase });
 
 //Database
 connection
-.authenticate()
-.then(() => {
-console.log("Conexão feita com o banco de dados!");
-})
-.catch((msgErro) => {
-console.log(msgErro);
-});
-
-app.get("/sobre", (req, res) => {
-    res.render("sobre");
+  .authenticate()
+  .then(() => {
+    console.log("Conexão feita com o banco de dados!");
+  })
+  .catch((msgErro) => {
+    console.log(msgErro);
   });
 
 // Estou dizendo para o Express usar o EJS como View engine
@@ -33,65 +29,58 @@ app.use(express.static("public"));
 // Middlewares
 middlewares(app);
 
-
 // Rotas
 app.get("/", async (req, res) => {
-    try {
-      const perguntas = await perguntaController.obterPerguntas();
-      res.render("index", { perguntas });
-    } catch (error) {
-      console.log(error);
-      res.status(500).send("Ocorreu um erro ao buscar as perguntas.");
-    }
-  });
+  try {
+    const perguntas = await perguntaController.obterPerguntas();
+    res.render("index", { perguntas });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Ocorreu um erro ao buscar as perguntas.");
+  }
+});
 
-  app.get("/perguntar", (req, res) => {
-    res.render("perguntar");
-  });
+app.get("/sobre", (req, res) => {
+  res.render("sobre");
+});
 
-  app.post("/salvarpergunta", async (req, res) => {
-    const { titulo, descricao } = req.body;
-  
-    try {
-      await perguntaRepository.salvarPergunta(titulo, descricao);
-      res.redirect("/");
-    } catch (error) {
-      console.log(error);
-      res.status(500).send("Ocorreu um erro ao salvar a pergunta.");
-    }
-  });
+app.get("/perguntar", (req, res) => {
+  res.render("perguntar");
+});
 
-  app.post("/salvarpergunta", async (req, res) => {
-    const { titulo, descricao } = req.body;
-  
-    await perguntaRepository.adicionarPergunta({ titulo, descricao });
-  
+app.post("/salvarpergunta", async (req, res) => {
+  const { titulo, descricao } = req.body;
+
+  try {
+    await perguntaRepository.salvarPergunta(titulo, descricao);
     res.redirect("/");
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Ocorreu um erro ao salvar a pergunta.");
+  }
+});
+
+app.get("/pergunta/:id", async (req ,res) => {
+  var id = req.params.id;
+  const pergunta = await Pergunta.findOne({ where: {id: id} });
+  if(pergunta != undefined) {
+    const respostas = await Resposta.findAll({ where: {perguntaId: pergunta.id}, order:[['id','DESC']] });
+    res.render("pergunta",{ pergunta: pergunta, respostas: respostas });
+  } else {
+    res.redirect("/");
+  }
+});
+
+app.post("/responder", (req, res) => {
+  var corpo = req.body.corpo;
+  var perguntaId = req.body.pergunta;
+  Resposta.create({
+    corpo: corpo,
+    perguntaId: perguntaId
+  }).then(() => {
+    res.redirect("/pergunta/"+perguntaId);
   });
-
-  
-  app.get("/pergunta/:id", async (req ,res) => {
-    var id = req.params.id;
-    const pergunta = await Pergunta.findOne({ where: {id: id} });
-    if(pergunta != undefined) {
-      const respostas = await Resposta.findAll({ where: {perguntaId: pergunta.id}, order:[['id','DESC']] });
-      res.render("pergunta",{ pergunta: pergunta, respostas: respostas });
-    } else {
-      res.redirect("/");
-    }
-  });
-
-  app.post("/responder", (req, res) => {
-    var corpo = req.body.corpo;
-    var perguntaId = req.body.pergunta;
-    Resposta.create({
-      corpo: corpo,
-      perguntaId: perguntaId
-    }).then(() => {
-      res.redirect("/pergunta/"+perguntaId);
-    });
-  });
-  
+});
 
 
-  module.exports = app;
+module.exports = app;
